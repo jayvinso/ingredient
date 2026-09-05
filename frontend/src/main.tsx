11d5be88582c37797c9e9b0c9ce371e2b78bce88
Jayden Vinson <vinson.103@buckeyemail.osu.edu>
2026-09-05 12:45:42 -0400

    Removed all of the placeholder code from the vite quickstart. Felt like it was bloaty.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
+  // strict mode just adds extra checks for our devleopment, keep for right now for testintg.
   <StrictMode>
     <App />
   </StrictMode>,
