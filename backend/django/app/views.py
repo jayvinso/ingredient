f788801991cf7f1b95df69dd337d73273dcd6a0d
Zilber5625git <kurt.wanner5625@gmail.com>
2026-09-16 16:04:08 -0400

    Minor docker change

--- backend/django/app/views.py
+++ backend/django/app/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from elasticsearch import Elasticsearch, helpers
+import os
+import json
+import re
+
+es = Elasticsearch('https://es01:9200', ca_certs="/usr/share/elasticsearch/config/certs/ca/ca.crt", 
+                       basic_auth=("elastic", "password"))
+
 def test(request):
-    return HttpResponse("Dummy data")
+
+    res = es.search(index="my-index", body= {"query":{
+        "sparse_vector": {
+        "field": "description",
+        "query_vector": { "salt": 5, "seasoning": 1}
+            }
+        }})
+    return HttpResponse(res["hits"]["hits"])
