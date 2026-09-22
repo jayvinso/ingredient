84528317cf284eefe9499de01fab69474b4d433c
Zilber5625git <kurt.wanner5625@gmail.com>
2026-09-22 16:30:16 -0400

    Reformatted files

--- backend/data_loader/load_recipes.py
+++ backend/data_loader/load_recipes.py
@@ -8,6 +8,31 @@ es = Elasticsearch('https://es01:9200', ca_certs="/usr/share/elasticsearch/confi
                        basic_auth=("elastic", "password"))
 
 try:
+    es.indices.delete(index="ingredients")
+except:
+    print("Creating index")
+
+settings = {
+}
+
+mappings = {
+    "properties": {
+        "title": {
+            "type": "text"
+        },
+    }
+}
+
+es.indices.create(index="ingredients", mappings=mappings, settings=settings)
+
+
+with open("ingredients.json") as file:
+    docs = json.loads(file.read())
+    # print(docs)
+    helpers.bulk(es, docs )
+
+
+try:
     es.indices.delete(index="my-index")
 except:
     print("Creating index")
@@ -29,7 +54,6 @@ mappings = {
 
 es.indices.create(index="my-index", mappings=mappings, settings=settings)
 
-print("Hello world")
 
 with open("recipes.json") as file:
     docs = json.loads(file.read())
